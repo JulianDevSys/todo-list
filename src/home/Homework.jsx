8a56a70dc1dfa7d982ba3e8b4901d427afe5b98b
julian castro <jucastrohenao@gmail.com>
2024-09-19 08:14:55 -0500

    terminado

--- src/home/Homework.jsx
+++ src/home/Homework.jsx
@@ -1,64 +1,170 @@
-import { useState } from "react"
-import "./styleHomework.css"
+import { useState } from "react";
+import "./styleHomework.css";
+/* me pasa id universales */
+import { v4 as uuid } from "uuid";
 
+export default function Homework() {
+  const [value, setValue] = useState(""); // el valor del input
+  const [todo, setTodo] = useState([]); // el valor de las tareas pendientes
+  const [inProcess, setProcess] = useState([]); //el valor de las tareas en procesp
+  const [finish, setFinish] = useState([]); //el valor de las tareas finzalizada
 
-export default function Homework(){
-    const [homeworks, setHomeworks]= useState([])
-    const [value, setValue]= useState("")
+  //cuando hagamos click--> tareas pendientes y las vamos a guardar
+  const handler = () => {
+    //funcion que recibe la listas tareas--> le pasamos un objeto
+    //el objeto--> la id : proporcionada para libreria, name: --> va a ser el valor del input
+  if([...todo,...inProcess,...finish].some(t=>t.name===value.trim())){
+    return alert('La tarea ya existe')
 
-    
+  }
+    setTodo([...todo, { id: uuid(), name: value.trim() }]);
+    setValue(""); // nos vacia el input
+  
+  };
 
-    const Handler= ()=>{
-        if(value.trim()){      
-        setHomeworks([...homeworks,value])
-        setValue(" ")
-        }
+  const toMove = (task, f, e) => {
+    f([...e, task]);
+  };
+  /* f--> muta el estado */
+  /* e--> el estado */
+  /* repasar mejor */
+  const deleteTask = (id, f, e) => {
+    f(e.filter((t) => t.id !== id));
+  };
 
-    }
-
-    return(
-        <div className="first_container">
-            <div className="header">
-                <input type="text" className="homework_todo" placeholder="escribe tus tareas" onChange={(e)=>{
-                    setValue(e.target.value)
-
-                }}/>
-                <div className="btn" onClick={(e)=>{
-                    Handler(e)
-
-                }}>Agregar tareas</div>
-            </div>
-
-                {/* contenedor principal */}
-            <div className="trabajos">
-                {/* segundo contenedor */}
-                <div className="work"> tareas pendientes
-                        <ul>
-                            {homeworks.map((tareas, index)=>( 
-                                <div className="tarea_container" key={index}>
-                                    <li className="tareas">{tareas}<div className="btn_process">en proceso</div></li>
-                                    
-                                    
-                                    </div>
-                            ))}
-                        </ul>
-
-                    </div>
-
-
-                <div className="work "> tareas en proceso
-
-
-                    </div>
-
-
-                <div className="work"> tareas finalizadas
-
-                    </div>
-
-                </div>
-
+  return (
+    <div className="first_container">
+      <div className="header">
+        <input
+          type="text"
+          className="homework_todo"
+          placeholder="escribe tus tareas"
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+        <div
+          className="btn"
+          onClick={(e) => {
+            handler(e);
+          }}
+        >
+          Agregar tareas
         </div>
-    )
+      </div>
 
+      <div className="trabajos">
+        <div className="work">
+          tareas pendientes
+          <ul className="container-tasks">
+            {/* mapeamos los valores que contienen las tareas pendientes */}
+            {todo.map((task) => (
+              <li className="task-item" key={task.id}>
+                <div className="tarea_container">
+                  <p>{task.name}</p>
+                  <div className="container-buttons">
+                    <div
+                      className="btn_process"
+                      onClick={() => {
+                        deleteTask(task.id, setTodo, todo);
+                        toMove(task, setProcess, inProcess);
+                      }}
+                    >
+                      hacer
+                    </div>
+                    <div
+                      className="btn_process"
+                      onClick={() => {
+                        deleteTask(task.id, setTodo, todo);
+                      }}
+                    >
+                      eliminar
+                    </div>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="work ">
+          tareas en proceso
+          <ul className="container-tasks">
+            {inProcess.map((task) => (
+              <li className="task-item" key={task.id}>
+                <div className="tarea_container">
+                  <p>{task.name}</p>
+                  <div className="container-buttons">
+                    <div
+                      className="btn_process"
+                      onClick={() => {
+                        deleteTask(task.id, setProcess, inProcess);
+                        toMove(task, setTodo, todo);
+                      }}
+                    >
+                      pendiente
+                    </div>
+                    <div
+                      className="btn_process"
+                      onClick={() => {
+                        deleteTask(task.id, setProcess, inProcess);
+                        toMove(task, setFinish, finish);
+                      }}
+                    >
+                      finalizar
+                    </div>
+                    <div
+                      className="btn_process"
+                      onClick={() => {
+                        deleteTask(task.id, setProcess, inProcess);
+                      }}
+                    >
+                      eliminar
+                    </div>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="work">
+          tareas finalizadas
+          <ul className="container-tasks">
+            {finish.map((task) => (
+              <li className="task-item" key={task.id}>
+                <div className="tarea_container">
+                  <p>{task.name}</p>
+                  <div className="container-buttons">
+                    <div
+                      className="btn_process"
+                      onClick={() => {
+                        deleteTask(task.id, setFinish, finish);
+                        toMove(task, setTodo, todo);
+                      }}
+                    >
+                      pendiente
+                    </div>
+                    <div className="btn_process" onClick={() => {
+                        deleteTask(task.id, setFinish, finish);
+                        toMove(task, setProcess, inProcess);
+                      }}>proceso</div>
+                    <div
+                      className="btn_process"
+                      onClick={() => {
+                        deleteTask(task.id, setFinish, finish);
+                      }}
+                    >
+                      eliminar
+                    </div>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
